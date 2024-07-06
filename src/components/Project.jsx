0a24d058424dd14e1e0a819945e9e1cdeae84bfa
dhinesh20022806 <dhinesh20022806@gmail.com>
2024-07-06 18:45:50 +0530

    delete icons folder

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,21 @@
+
+import styled from 'styled-components';
+import BoxWrapper from './BoxComponent';
+
+const ProjectSection = styled.section`
+width:100%;
+height:fit-content;
+display:flex;
+flex-direction:column;
+padding:30px;
+
+`;
 export default function Project(){
-	return (<div>
-		<p>Projects </p>
-	</div>)
+	return (<ProjectSection>
+		<BoxWrapper $backgroundColor={"#f5f5f7"} >
+
+		First Project
+		</BoxWrapper>
+		
+	</ProjectSection>)
 }
