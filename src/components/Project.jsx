511bad216f64c9e50f1e59f0ccc452852e192664
dhinesh20022806 <dhinesh20022806@gmail.com>
2024-07-06 19:03:07 +0530

    project_images folder added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,7 @@
 
 import styled from 'styled-components';
 import BoxWrapper from './BoxComponent';
+import Title from './Title'
 
 const ProjectSection = styled.section`
 width:100%;
@@ -10,11 +11,24 @@ flex-direction:column;
 padding:30px;
 
 `;
+
+const ButtonWrapper = styled.div`
+display:flex;
+justify-content:space-between;
+gap:30px;
+`;
 export default function Project(){
 	return (<ProjectSection>
-		<BoxWrapper $backgroundColor={"#f5f5f7"} >
+		<BoxWrapper $backgroundColor={"#f5f5f7"} $display={'flex'} $gap={30}  $flexDirection={'column'} $justifyContent={"center"} $alignItems={"center"} >
 
-		First Project
+		<img src="" alt="project-title" />
+
+		<Title>React Quiz App </Title>
+
+		<ButtonWrapper> 
+			<button>Preview</button>
+			<button>Github code</button>
+		 </ButtonWrapper>
 		</BoxWrapper>
 		
 	</ProjectSection>)
